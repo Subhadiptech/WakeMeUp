216069ffb747ff345b72c6e434d7de1599715d5d
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-09-27 02:49:44 +0530

    first commit

---
+++ app/src/main/java/com/ersubhadip/wakemeup/SetTimeActivity.java
@@ -0,0 +1,15 @@
+package com.ersubhadip.wakemeup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_time);
+
+    }
+}
